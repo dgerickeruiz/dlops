f3526931c7a38206c9d335e6dd92b3ead8d782ff
dgerickeruiz <dnl991@gmail.com>
2025-07-01 18:35:24 -0400

    primer modelo video 4

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import pytorch_lightning as pl
 import yaml
 import sys
+import importlib
+
 
 config = {
     'datamodule': {
@@ -11,19 +13,42 @@ config = {
     },
     'trainer': {
         'max_epochs': 10,
-        'logger': None,
         'enable_checkpointing': False,
         'overfit_batches': 0
-    }
+    },
+    'logger': None,
+    'callbacks': None
 }
 
 
 def train(config):
     dm = MNISTDataModule(**config['datamodule'])
     module = MNISTModule(config)
+    # configure logger
+    if config['logger'] is not None:
+        if config['logger'] == 'WandbLogger':
+            config['trainer']['logger'] = getattr(pl.loggers, config['logger'])(
+                **config['logger_params'], config=config)
+        else:
+            config['trainer']['logger'] = getattr(
+                pl.loggers, config['logger'])(**config['logger_params'])
+    # configure callbacks
+    if config['callbacks'] is not None:
+        callbacks = []
+        for callback in config['callbacks']:
+            if callback['name'] == 'WandBCallback':
+                dm.setup()
+                callback['params']['dl'] = dm.val_dataloader()
+            elif callback['name'] == 'ModelCheckpoint':
+                callback['params']['filename'] = f'{callback["params"]["filename"]}-{{val_loss:.5f}}-{{epoch}}'
+            cb = getattr(importlib.import_module(callback['lib']), callback['name'])(
+                **callback['params'])
+            callbacks.append(cb)
+            config['trainer']['callbacks'] = callbacks
+    # train
     trainer = pl.Trainer(**config['trainer'])
     trainer.fit(module, dm)
-    trainer.save_checkpoint('final.ckpt')
+    trainer.save_checkpoint('checkpoints/final.ckpt')
 
 
 if __name__ == '__main__':
